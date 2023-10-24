b9f447e4c6e6f128d858548469507dff46bc07be
marviph <109024626+marviph@users.noreply.github.com>
2023-10-24 12:49:41 +0300

    Добавлен файл activity.sql

--- activity.sql
+++ activity.sql
@@ -1,11 +1,31 @@
-SELECT p.ProgramAttribute, sp.SubProgramAttribute, ar.AreaAttribute, pr.ProjectAttribute, pt.PointAttribute, c.ContractAttribute, pt2.PointAttribute
-FROM Activity a
+WITH RECURSIVE ActivityHierarchy AS (
+  SELECT a.ActivityId, a.ActivityTypeId, a.ParentId, '' AS Path, 0 AS Level
+  FROM Activity a
+  WHERE a.ActivityId = COALESCE(pn_activityid, 1)
+  
+  UNION ALL
+  
+  SELECT a.ActivityId, a.ActivityTypeId, a.ParentId, CONCAT(h.Path, ' -> ', a.ActivityTypeId) AS Path, h.Level + 1 AS Level
+  FROM Activity a
+  INNER JOIN ActivityHierarchy h ON a.ParentId = h.ActivityId
+)
+SELECT h.Path, 
+  CASE 
+    WHEN a.ActivityTypeId = 1 THEN p.ProgramAttribute
+    WHEN a.ActivityTypeId = 2 THEN sp.SubProgramAttribute
+    WHEN a.ActivityTypeId = 3 THEN pr.ProjectAttribute
+    WHEN a.ActivityTypeId = 4 THEN c.ContractAttribute
+    WHEN a.ActivityTypeId = 5 THEN pt.PointAttribute
+  END AS EntityAttribute,
+  ar.AreaAttribute,
+  pt2.PointAttribute
+FROM ActivityHierarchy h
+LEFT JOIN Activity a ON a.ActivityId = h.ActivityId
 LEFT JOIN Program p ON p.ProgramId = a.ActivityId
 LEFT JOIN SubProgram sp ON sp.SubProgramId = a.ActivityId
 LEFT JOIN Project pr ON pr.ProjectId = a.ActivityId
 LEFT JOIN Contract c ON c.ContractId = a.ActivityId
+LEFT JOIN Point pt ON pt.PointId = a.ActivityId
 LEFT JOIN Area ar ON ar.AreaId = c.AreaId
-LEFT JOIN Point pt ON pt.PointId = pr.ProjectId
 LEFT JOIN Point pt2 ON pt2.PointId = c.ContractId
-WHERE a.ActivityTypeId IN (1, 2, 3, 4, 5)
-ORDER BY a.ActivityId;
+ORDER BY h.Level;
