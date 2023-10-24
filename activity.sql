4f7173e887ee6568c53563122d627eeb032e8c87
marviph <109024626+marviph@users.noreply.github.com>
2023-10-24 12:39:14 +0300

    Добавлен файл activity.sql

--- activity.sql
+++ activity.sql
@@ -1,15 +1,11 @@
--- рекурсия 
-WITH RECURSIVE r AS (
-    SELECT 
-        1 AS i, 
-        1 AS factorial
-    
-    UNION 
-
-    SELECT 
-        i+1 AS i, 
-        factorial * (i+1) as factorial 
-    FROM r
-    WHERE i < 10
-)
-SELECT * FROM r;
+SELECT p.ProgramAttribute, sp.SubProgramAttribute, ar.AreaAttribute, pr.ProjectAttribute, pt.PointAttribute, c.ContractAttribute, pt2.PointAttribute
+FROM Activity a
+LEFT JOIN Program p ON p.ProgramId = a.ActivityId
+LEFT JOIN SubProgram sp ON sp.SubProgramId = a.ActivityId
+LEFT JOIN Project pr ON pr.ProjectId = a.ActivityId
+LEFT JOIN Contract c ON c.ContractId = a.ActivityId
+LEFT JOIN Area ar ON ar.AreaId = c.AreaId
+LEFT JOIN Point pt ON pt.PointId = pr.ProjectId
+LEFT JOIN Point pt2 ON pt2.PointId = c.ContractId
+WHERE a.ActivityTypeId IN (1, 2, 3, 4, 5)
+ORDER BY a.ActivityId;
