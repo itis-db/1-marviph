cf5ccee2195d233c8b4f7317d22069f8c40e53c9
marviph <109024626+marviph@users.noreply.github.com>
2023-10-24 12:37:53 +0300

    Добавлен файл data.sql

--- data.sql
+++ data.sql
@@ -1,17 +1,23 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+INSERT INTO ActivityType (ActivityTypeId, ActivityTypeAttribute)
+VALUES (1, 'Program'), (2, 'Sub Program'), (3, 'Project'), (4, 'Contract'), (5, 'Point');
+
+INSERT INTO Area (AreaId, AreaAttribute)
+VALUES (1, 'Area 1'), (2, 'Area 2');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId)
+VALUES (1, NULL, 1), (2, 1, 2), (3, 2, 3), (4, 3, 4), (5, 3, 5);
+
+INSERT INTO Program (ProgramId, ProgramAttribute)
+VALUES (1, 'Program 1');
+
+INSERT INTO SubProgram (SubProgramId, SubProgramAttribute)
+VALUES (2, 'Sub Program 2');
+
+INSERT INTO Project (ProjectId, ProjectAttribute)
+VALUES (3, 'Project 3');
+
+INSERT INTO Contract (ContractId, ContractAttribute, AreaId)
+VALUES (4, 'Contract 4', 1);
+
+INSERT INTO Point (PointId, PointAttribute)
+VALUES (5, 'Point 5');
